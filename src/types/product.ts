c4fe7c6f82f3f6534dabb2ae5a18941fdde5ee01
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-08-29 00:56:14 +0200

    Add product ype

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+interface Product {
+  id: string;
+  title: string;
+  description?: string;
+  category?: string;
+  price: string;
+  image: string;
+  rating?: {
+    count: number,
+    rate: number
+  };
+}
+
+export default Product;
