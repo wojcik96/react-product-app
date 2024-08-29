32223ffce97baf24ab54c7835d956ecf0a434b52
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-08-29 11:12:15 +0200

    Integrate useLastViewedProduct hook and SectionWrapper in HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
+import SectionWrapper from "../components/sectionWrapper/SectionWrapper";
+import useLastViewedProduct from "../hooks/useLastViewedProduct";
+
 function HomePage() {
-    return ( 
-        <h1>Home Page </h1>
-     );
+  const { lastViewedProduct } = useLastViewedProduct();
+  
+  return (
+    <SectionWrapper title="Home Page">
+      {!!lastViewedProduct && (
+        <Link to={`/products/${lastViewedProduct}`}>
+          Wróć do przeglądania produktu
+        </Link>
+      )}
+    </SectionWrapper>
+  );
 }
 
 export default HomePage;
