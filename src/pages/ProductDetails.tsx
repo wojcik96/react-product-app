5372426745ff26b289e2c9b8490f88df50cf2db2
Arkadiusz Wojcik <arkadiuszwojcik15@wp.pl>
2024-08-29 11:12:47 +0200

    Add product details page with data fetching, last viewed product tracking, and status messages

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,13 +1,52 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function ProductDetailsPage() {
-  const { id } = useParams();
+import useFetch from "../hooks/useFetch";
+import { fetchProductDetails } from "../api/productsApi";
+import SectionWrapper from "../components/sectionWrapper/SectionWrapper";
+import StatusMessage from "../components/statusMessage/StatusMessage";
+import classes from "./ProductDetails.module.css";
+import useLastViewedProduct from "../hooks/useLastViewedProduct";
+
+function ProductDetails() {
+  const { setLastViewedProduct } = useLastViewedProduct();
+  const { id } = useParams<{ id: string }>();
+
+  if (!id) return <StatusMessage noId={!id} />;
+
+  useEffect(() => {
+    setLastViewedProduct(id);
+  }, [id]);
+
+  const { isFetching, fetchedData: product, error} = useFetch(() => fetchProductDetails(id));
+
+  if (isFetching || error || !product) {
+    return (
+      <StatusMessage isLoading={isFetching} error={error} noData={!product} />
+    );
+  }
+
+  const { title, image, price, description, category, rating } = product;
 
   return (
-    <div>
-      <h1> Product ID: {id}</h1>
-    </div>
+    <SectionWrapper title={title}>
+      <>
+        <img
+          className={classes.productDetails__image}
+          src={image}
+          alt={title}
+        />
+        <p className={classes.productDetails__price}>Price: {price} PLN</p>
+        <p className={classes.productDetails__description}>
+          Description: {description}
+        </p>
+        <p className={classes.productDetails__category}>Category: {category}</p>
+        <p className={classes.productDetails__rating}>
+          Rating: {rating?.rate} ({rating?.count} votes)
+        </p>
+      </>
+    </SectionWrapper>
   );
 }
 
-export default ProductDetailsPage;
+export default ProductDetails;
